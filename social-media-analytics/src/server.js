d1b543985d49dc2d4d24332f31e3286fe36227f9
AnkitKumar068 <ankitkumar.12a.13@gmail.com>
2025-04-02 20:29:07 +0530

    backend of social media analytics

---
+++ social-media-analytics/src/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // Middleware to parse JSON
+app.use("/api", routes); // Use defined routes
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
